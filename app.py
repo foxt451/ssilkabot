3abb64dbcde89295f00d07f4b1ab8d123e11b388
foxt451 <52706110+foxt451@users.noreply.github.com>
2020-11-27 03:42:05 +0200

    ''

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ async def on_startup(dp):
     filters.setup(dp)
     middlewares.setup(dp)
 
-    from utils.notify_admins import on_startup_notify
-    await on_startup_notify(dp)
-
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
